7979233d456b10ddf02d4eb6c3b2180e135f7928
Salocin0 <nicolasarieldurelli@gmail.com>
2024-10-17 19:33:50 -0300

    primer commit

---
+++ src/components/BtnCategoria.jsx
@@ -0,0 +1,10 @@
+//estilos del boton
+const BtnCategoria = ({ texto, categoria, funcionPadre }) => {
+  const handleSetCategoria2 = () => {
+    funcionPadre(categoria);
+  };
+
+  return <button onClick={handleSetCategoria2}>{texto}</button>;
+};
+
+export default BtnCategoria;
